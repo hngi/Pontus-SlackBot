131022ecf63cef5c1e842780224ae79066a4f5f9
J-Engineers <55509227+J-Engineers@users.noreply.github.com>
2019-09-24 07:23:03 +0100

    Check user permissions in the server.
    Making connection to database only when user is permitted to interact with database.

--- config.php
+++ config.php
@@ -3,6 +3,18 @@
     $user = "root";
     $password = "";
     $dbname = "hng_login";
+    //     making sure server responds to user
+	$link = new mysqli($host,$user,$password);
+	if (!$link) {
+		die('Could not connect: ' . mysqli_error($link));
+	}
+    //     making sure user contain database
+	$db_selected = mysqli_select_db($link, $dbname);
+	if ($db_selected) {
+         $conn = mysqli_connect("$host", "$user", "$password", "$dbname");
+    }else{
+        die('Could not find database, Kindly make the database accessible by this user');
+    }
 
-    $conn = mysqli_connect("$host", "$user", "$password", "$dbname"); 
+    
 ?>
