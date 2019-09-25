83c624ebb9aa46cef8f7b30c8c4a8d608e79dd48
unknown <fegoworks@hotmail.com>
2019-09-25 14:35:04 +0100

    feature(bot): function to push messages to server and provide feedback

--- app/index.js
+++ app/index.js
@@ -1,25 +1,30 @@
 const SlackBot = require('slackbots');
-const botToken = 'xoxb-771056643120-773247034999-keaJulkCWhAJq2Jw2ha10m7s'
+const axios = require('axios')
+const dotenv = require('dotenv')
 const uuidv4 = require('uuid/v4');
+
+dotenv.config()
 
 // Initialize conversations to empty
 let conversations = [];
 
 const bot = new SlackBot({
-  token: botToken,
+  token: `${process.env.BOT_TOKEN}`,
   name: 'saveconvo',
 });
 
 // Need a function to determine the current channel
-// Need a function to get the user that initiated the call
+
 
 bot.on('start', () => {
   const params = {
-    icon_emoji: ':smiley:',
+    icon_emoji: ':robot_face:',
   };
 
   bot.postMessageToChannel('general', 'Here to save your convo', params);
 });
+
+
 
 
 // Error Handler
@@ -36,17 +41,82 @@ bot.on('message', (data) => {
 
 
 // Reaponse to data
-const handleMessage = (data) => {
+const handleMessage = async (data) => {
   let message = data.text;
-  // let userId = data.user;
+
   if (message.includes(' save my convo')) {
-    // let username = await bot.getUser(userId)
+    let id = data.user;
+    const users = await bot.users;
+    const user = users.find(user => {
+      return user.id == id
+    })
+
+    const useremail = user.profile.email
+
     let newMessage = {
       id: uuidv4(),
+      token: `${process.env.SERVER_TOKEN}`,
       message: message.replace(' save my convo', ''),
+      email: useremail
     }
 
     conversations.push(newMessage)
+    sendConvo(newMessage)
+    messageSaved()
+
+  } else if (message.includes(' help')) {
+    runHelp()
   }
   return console.log(conversations)
+}
+
+// Get channel
+const getChannel = () => {
+
+}
+
+// Push messages to server
+const sendConvo = (data) => {
+  let url = ''
+  let axiosConfig = {
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+      "Access-Control-Allow-Origin": "*",
+    }
+  };
+
+  axios.post(`${url}`, data, axiosConfig)
+    .then((res) => {
+      console.log("RESPONSE RECEIVED: ", res);
+    })
+    .catch((err) => {
+      console.log("AXIOS ERROR: ", err);
+    })
+}
+
+// Show Help
+const runHelp = () => {
+  const params = {
+    icon_emoji: ':question:'
+  }
+
+  bot.postMessageToChannel(
+    'general',
+    `Type *@saveconvo* with *save my convo* then paste the contents 
+    you want to save and *help* to get this instruction again`,
+    params
+  );
+}
+
+// Prompt on save
+const messageSaved = () => {
+  const params = {
+    icon_emoji: ':smile:'
+  }
+
+  bot.postMessageToChannel(
+    'general',
+    `Your message has been saved successfully`,
+    params
+  );
 }
